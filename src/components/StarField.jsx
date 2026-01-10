38e45440e8981276d8ff0e4d834617aa6176cdb7
sim0nvictor <ineedmoneyineedmoney01@gmail.com>
2026-01-10 17:10:54 +0000

    Deploy to gh-pages

--- src/components/StarField.jsx
+++
@@ -1,9 +0,0 @@
-export default function StarField() {
-  return (
-    <div className="absolute inset-0 overflow-hidden pointer-events-none">
-      <div className="stars" />
-      <div className="stars2" />
-      <div className="stars3" />
-    </div>
-  );
-}
